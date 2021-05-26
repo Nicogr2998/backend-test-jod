1b398b4ffefb244708a4dbb1dfdb465fad962852
nicolas <nicogr2998@gmail.com>
2021-05-26 20:23:32 -0300

    app3

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use(function(_req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", false);
+    res.header("Access-Control-Allow-Origin:http://localhost:4200/", '*');
+    res.header("Access-Control-Allow-Credentials", true);
     
     next();
 });
