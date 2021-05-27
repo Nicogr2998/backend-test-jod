1c2081fe524aeb63f7bf0e16760844e951a86dc3
nicolas <nicogr2998@gmail.com>
2021-05-26 21:59:13 -0300

    app

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ app.use(function(_req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.header("Access-Control-Allow-Credentials", true);
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-   
+    res.setHeader('Access-Control-Allow-Methods', ' GET');
+    
     next();
 });
 
