a7b10561c9df8a63a9985bb1d00f3855cb8bab20
nicolas <nicogr2998@gmail.com>
2021-05-26 22:05:44 -0300

    app

--- app.js
+++ app.js
@@ -11,10 +11,8 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use(function(_req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*','localhost');
     res.header("Access-Control-Allow-Credentials", true);
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Methods', ' GET');
     
     next();
 });
